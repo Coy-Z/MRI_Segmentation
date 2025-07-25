51a984e668be1c4b51d2200242bd765bf9bc088a
Coy Zhu <ckz20@cam.ac.uk>
2025-07-25 11:16:24 +0100

    modified directory

--- tools_and_tests/ndarray_cropper.py
+++ tools_and_tests/ndarray_cropper.py
@@ -1,5 +1,5 @@
 import numpy as np
 
-arr = np.load('../data/data2/mag.npy')[67:67+134,140:140+155,:65]
+arr = np.load('data/data2/mag.npy')[67:67+134,140:140+155,:65]
 
-np.save('../data/magnAorta.npy', arr, allow_pickle = False)
+np.save('data/magnAorta.npy', arr, allow_pickle = False)
